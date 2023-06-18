28490cab5f01f5575b38ef58d270bc2657b95768
ToCraft <tobias.cuiper@gmx.de>
2023-06-18 10:02:45 +0200

    added got support

--- src/main/java/unlocker/STRScanUnlocker.java
+++ src/main/java/unlocker/STRScanUnlocker.java
@@ -2,12 +2,12 @@ package unlocker;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
-import lotr.common.command.LOTRCommandStrScan;
+import got.common.command.GOTCommandStrScan;
 
-@Mod(modid = "unlocker", dependencies = "required-after:lotr")
+@Mod(modid = "unlocker", dependencies = "required-after:got")
 public class STRScanUnlocker {
 	@Mod.EventHandler
 	public void serverStarting(FMLServerStartingEvent e) {
-		e.registerServerCommand(new LOTRCommandStrScan());
+		e.registerServerCommand(new GOTCommandStrScan());
 	}
 }
